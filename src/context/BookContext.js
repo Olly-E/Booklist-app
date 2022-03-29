d18ac6799238487dad7182a7daa90f31bc191339
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-03-29 21:39:27 +0100

    setting up local storage

--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,19 +1,23 @@
-import React, {createContext, useReducer} from 'react';
+import React, {createContext, useReducer, useEffect} from 'react';
 import {v1, v4} from 'uuid';
 import { bookReducer } from '../reducers/bookReducer';
 
 export  const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const [books, dispatch] = useReducer(bookReducer, []);
+    const [books, dispatch] = useReducer(bookReducer, [], () => {
+      const localData = localStorage.getItem('books');
+      return localData ? JSON.parse(localData) : [];
+    });
+    useEffect(() => {
+      localStorage.setItem('books', JSON.stringify(books))
+    }, [books])
    
   return (
     <BookContext.Provider value={{books, dispatch}}>
         {props.children}
     </BookContext.Provider>
-        
-    
   )
 }
 
-export default BookContextProvider
+export default BookContextProvider 
